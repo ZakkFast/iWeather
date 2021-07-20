8407df7ec7010d6e45bb779d96ff8c76c1bb6a71
ZakkFast <Zakkfastpro@gmail.com>
2021-07-19 17:36:48 -0700

    implemented forecast function

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,15 +1,19 @@
+//API key for easy recall. Normally would hide senstive info but its a weather API. ::shrugs::
 const APIkey = '25e58f3366c8c00378ddd7ecaf34643b'
 // function kicked off by button clicks when the document is ready
 $(document).ready(function(){
     $('#searchBtn').on('click', function(){
-        console.log('button click ln4')
+        //collects user input and stores it in searchInput for API calls.
         let searchInput = $('#searchInput').val()
         console.log(searchInput, 'searchInput 6')
+        //Clears form after submission
         // $('#searchInput').val('')
-
+        // console.log(`https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=${APIkey}&units=imperial`, 'tesdt')
         getWeather(searchInput)
         createHistoryBtnEl(searchInput)
+        getForcast(searchInput)
     })
+    //Initial call to API for current weather.
     const getWeather = (searchInput) => {
         console.log(searchInput, 'searchInput')
         $.ajax({
@@ -17,16 +21,18 @@ $(document).ready(function(){
             url: `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${APIkey}&units=imperial`,
             dataType: 'json',
             success: function(data){
+                // console.log(data.list.length)
+                //Creating HTML Elements and populating with data from API
                 let title = $(`<h2 class="card-title">${searchInput}</h2>`) //add date to title
                 let card = $(`<div class='card'></div>`)
                 let wind = $(`<p class='card-text'>Wind Speed: ${data.wind.speed} mph</p>`)
-                let humidity = $(`<p class='card-text'>Humidty: ${data.main.humidity}</p>`)
+                let humidity = $(`<p class='card-text'>Humidty: ${data.main.humidity}%</p>`)
                 let temp = $(`<p class='card-text'>Temp: ${data.main.temp} °F</p>`)
-                // http://openweathermap.org/img/w/" + iconcode + ".png"
                 let icon = $(`<img src='http://openweathermap.org/img/w/${data.weather[0].icon}.png'>`)
                 let cardBody =$(`<div class='card-body'>`)
-                console.log(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${APIkey}&units=imperial`)
+                //console log for API data for testing purposes
                 console.log(data)
+                //Appending elements to page
                 title.append(icon)
                 cardBody.append(title, temp, wind, humidity)
                 card.append(cardBody)
@@ -36,12 +42,41 @@ $(document).ready(function(){
             }
         })
     }
+    const getForcast = (searchInput) =>{
+        $.ajax({
+            type: 'GET',
+            //Use correct api dumbass
+            url: `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=${APIkey}&units=imperial`,
+            dataType: 'json',
+            success: function(data){
+                //Grabbing forecast class, adding a header, appending div class row to append to in for loop
+                $('#forecast').html('<h3 class="mt-3>Forecast</h3>').append('<div class="row"')
+                console.log(data, 'test2')
+                console.log(data.list[0].dt_txt)
+                for(let i = 0; i < data.list.length; i++){
+                    if(data.list[i].dt_txt.indexOf('15:00:00') !== -1){
+                        let col = $('<div class="col-md-2"></div>')
+                        let card = $('<div class="card bg-primary text-white"</div>')
+                        let body =$('<div class="card-body p-2"</div>')
+                        let title = $('<h4 class="title-card"></h4>').text(new Date(data.list[i].dt_txt).toLocaleDateString())
+                        // let img = $(`<img src='http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`)
+                        let forTemp = $('<p class="card-text"></p>').text(`High of: ${data.list[i].main.temp_max}°F`)
+                        let forHumidity = $('<p class="card-text></p>').text(`Humidty: ${data.list[i].main.humidity}%`)
+ 
+                        col.append(card.append(body.append(title, forTemp, forHumidity)));
+                        $('#forecast').append(col);
+                    }
+                }
+            }
+        })
+    }
+    //Function to create and append elements to history for recall
     function createHistoryBtnEl (searchInput){
         var li = $('<li>').addClass('list-group-item list-group-item-action').text(searchInput)
         li.on('click', btnclick)
         $('.history').append(li)
     }
-    
+    //Function to allow history li elements to be clicked and recall the api for current info and forcast.
     function btnclick(e){
         console.log('btn clicked ln 34', e.currentTarget.innerText)
         let historyText = e.currentTarget.innerText
